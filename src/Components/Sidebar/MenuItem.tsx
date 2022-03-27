409f125c0d16a39013f71f600e0176b1a45019a7
Oscar Lopez <oscareduardolp6@gmail.com>
2022-03-26 20:35:31 -0600

    Change styling in MenuItem File

--- src/Components/Sidebar/MenuItem.tsx
+++ src/Components/Sidebar/MenuItem.tsx
@@ -2,4 +2,6 @@ import React, { FC } from 'react'
 import { MenuItemsProps } from './MenuItemsTypes'
 
 export const MenuItem: FC<MenuItemsProps> = ({className, children, ...props}) => 
-  <a className={`card has-background-dark has-text-primary py-2 mt-5 ${className}`} {...props}>{ children }</a>
+  <a className={`card has-background-dark has-text-primary py-2 mt-5 myOption ${className}`} {...props}>
+    { children }
+  </a>
