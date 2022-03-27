2e09dcf64a346c948fc891231b2a78831c6baa0f
Oscar Lopez <oscareduardolp6@gmail.com>
2022-03-26 20:56:53 -0600

    Added React Router routes & First Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Header } from "./Components/Header/Header"
 import { SideBar } from "./Components/Sidebar/SideBar"
+import { Routes, Route } from 'react-router-dom'
+import { Sales } from "./Routes/Sales"
 
 import './Sass/mySassStyles.sass'
 
 function App() {
   return (
     <>
+      <Routes>
+        <Route path='/' element={<Sales/>} />
+      </Routes>
       <SideBar/>
       <Header/>
     </>
