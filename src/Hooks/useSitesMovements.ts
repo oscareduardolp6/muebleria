37d4f1235048fde142bbdbcacece10ad2d6f86ce
Oscar Lopez <oscareduardolp6@gmail.com>
2022-05-21 16:45:38 -0500

    Refactorizado reducer a un solo return

--- src/Hooks/useSitesMovements.ts
+++ src/Hooks/useSitesMovements.ts
@@ -14,71 +14,64 @@ export type ActionPayload = {
   state?: myStateType
 }
 
+const validateQuantitys = (showQuantity: number, privateQuantity: number) => {
+
+
+}
+
 export const siteMovementsReducer = (product: myStateType, payload: ActionPayload): typeof product => {
   if(!product) return 
   
   const { showSiteQuantity = 0, privateSiteQuantity = 0 } = product
+  let newPublicSiteQuantity, newPrivateSiteQuantity : number
 
   switch(payload.action){
     case 'incrementPublicSiteQuantity' : {
-      let newPublicSiteQuantity = showSiteQuantity + 1
+      newPublicSiteQuantity = showSiteQuantity + 1
       const total = showSiteQuantity  + privateSiteQuantity 
       newPublicSiteQuantity = newPublicSiteQuantity > total ? total : newPublicSiteQuantity
       newPublicSiteQuantity = newPublicSiteQuantity < 0 ? 0 : newPublicSiteQuantity
       const diff = total - newPublicSiteQuantity
-      let newPrivateSiteQuantity = diff < 0 ? 0 : diff
-      return {
-        ...product, 
-        privateSiteQuantity: newPrivateSiteQuantity, 
-        showSiteQuantity: newPublicSiteQuantity
-      }
+      newPrivateSiteQuantity = diff < 0 ? 0 : diff
     }
+    break 
     case 'decrementPublicSiteQuantity': {
-      let newPublicSiteQuantity = showSiteQuantity - 1
+      newPublicSiteQuantity = showSiteQuantity - 1
       const total = showSiteQuantity  + privateSiteQuantity
       newPublicSiteQuantity = newPublicSiteQuantity > total ? total : newPublicSiteQuantity
       newPublicSiteQuantity = newPublicSiteQuantity < 0 ? 0 : newPublicSiteQuantity
       const diff = total - newPublicSiteQuantity
-      const newPrivateSiteQuantity = diff < 0 ? 0 : diff
-      // const newPrivateSiteQuantity = total - newPublicSiteQuantity
-      return {
-        ...product, 
-        privateSiteQuantity: newPrivateSiteQuantity, 
-        showSiteQuantity: newPublicSiteQuantity
-      }
+      newPrivateSiteQuantity = diff < 0 ? 0 : diff
     }
+    break
     case 'incrementPrivateSiteQuantity': {
-      let newPrivateSiteQuantity = privateSiteQuantity + 1
+      newPrivateSiteQuantity = privateSiteQuantity + 1
       const total = showSiteQuantity  + privateSiteQuantity
       newPrivateSiteQuantity = newPrivateSiteQuantity > total ? total : newPrivateSiteQuantity
       newPrivateSiteQuantity = newPrivateSiteQuantity < 0 ? 0 : newPrivateSiteQuantity
       const diff = total - newPrivateSiteQuantity
-      const newPublicSiteQuantity = diff < 0 ? 0 : diff
-      // const newPublicSiteQuantity = total - newPrivateSiteQuantity
-      return {
-        ...product, 
-        privateSiteQuantity: newPrivateSiteQuantity, 
-        showSiteQuantity: newPublicSiteQuantity
-      }
+      newPublicSiteQuantity = diff < 0 ? 0 : diff
     }
+    break
     case 'decrementPriveteSiteQuantity': {
-      let newPrivateSiteQuantity = privateSiteQuantity  - 1
+      newPrivateSiteQuantity = privateSiteQuantity  - 1
       const total = showSiteQuantity  + privateSiteQuantity
       newPrivateSiteQuantity = newPrivateSiteQuantity > total ? total : newPrivateSiteQuantity
       newPrivateSiteQuantity = newPrivateSiteQuantity < 0 ? 0 : newPrivateSiteQuantity
       const diff = total - newPrivateSiteQuantity
-      const newPublicSiteQuantity = diff < 0 ? 0 : diff 
-      return {
-        ...product, 
-        privateSiteQuantity: newPrivateSiteQuantity, 
-        showSiteQuantity: newPublicSiteQuantity
-      }
+      newPublicSiteQuantity = diff < 0 ? 0 : diff 
     }
+    break
     case 'setProduct': {
       return {
         ...payload.state 
       }
     }
+  }
+  return {
+    ...product, 
+    privateSiteQuantity: newPrivateSiteQuantity, 
+    showSiteQuantity: newPublicSiteQuantity
   }
 
 }
