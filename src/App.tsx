8a9854aead12062effbbab722171e814c22efb19
Oscar Lopez <oscareduardolp6@gmail.com>
2022-05-27 00:04:16 -0500

    Corrección CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
+import './Sass/mySassStyles.sass'
+
 import { Header } from "./Components/Header"
 import { SideBar } from "./Components/Sidebar/SideBar"
 import { Routes, Route } from 'react-router-dom'
-import { Sales } from "./Routes/Sales"
 
 import { CreateProduct } from "./Routes/CreateProduct"
 import ClientsManager from "./Routes/ClientsManager/"
 import Transactions from "./Routes/Transactions/"
 
-import './Sass/mySassStyles.sass'
 import { SitesMovements } from "./Routes/SitesMovements/SitesMovements"
 import Purchases from "./Routes/Purchases"
 import ProductManager from "./Routes/ProductManager"
@@ -20,7 +20,6 @@ function App() {
       <Header/>
       <Routes>
         <Route path='/productManager' element={<CreateProduct />} />
-        {/* <Route path='/sales' element={<Sales />} /> */}
         <Route path='/clientsManager' element={<ClientsManager />} />
         <Route path='/transactions' element={<Transactions/>} />
         <Route path='/siteMovs' element={<SitesMovements />}/>
