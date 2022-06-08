64440ca353ec36b666d30c538e3e6d385f5b7d7f
Oscar Lopez <oscareduardolp6@gmail.com>
2022-06-07 23:03:47 -0500

    Componente de atribución en componente separado

---
+++ src/Routes/Transactions/FlaticonAttribution.tsx
@@ -0,0 +1,4 @@
+export const FlaticonAttribution = () => 
+  <a className='title is-7 has-text-white ml-5'  href="https://www.flaticon.com/free-icons/couch" title="couch icons">
+    Couch icons created by Hilmy Abiyyu A. - Flaticon
+  </a>
